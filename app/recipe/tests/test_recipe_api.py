913f70faa7462037d32e477aa03654e9a9964ba3
4nt0nn <antonstalhandske@gmail.com>
2019-12-31 15:37:08 +0100

    Added custom recipe detail view.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,12 +5,45 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPE_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """Return recipe detail url
+
+    Arguments:
+        recipe_id {[type]} -- [description]
+    """
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
+
+def sample_tag(user, name='Main course'):
+    """Create and return a sample tag
+
+    Arguments:
+        user {[type]} -- [description]
+
+    Keyword Arguments:
+        name {str} -- [description] (default: {'Main course'})
+    """
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Cinnamon'):
+    """Create and return a sample ingredient
+
+    Arguments:
+        user {[type]} -- [description]
+
+    Keyword Arguments:
+        name {str} -- [description] (default: {'Cinnamon'})
+    """
+    return Ingredient.objects.create(user=user, name=name)
 
 
 def sample_recipe(user, **params):
@@ -93,4 +126,17 @@ class PrivateRecipeApiTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detial(self):
+        """Test viewing a recipe details
+        """
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+
+        res = self.client.get(url)
+        serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
